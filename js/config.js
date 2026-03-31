480b736feba98ee3c48adf58460a24115b8c9901
ali0223jjang <ali@inbl.io>
2026-03-31 09:24:27 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://cprtovccjnfuqaqtidyr.supabase.co';
+const SUPABASE_ANON_KEY = '여기에_anon_key_붙여넣기';
+const ADMIN_TOTP_SECRET = 'YOUR_TOTP_SECRET_HERE';
+const ADMIN_PW_HASH = 'YOUR_SHA256_HASH_HERE';
