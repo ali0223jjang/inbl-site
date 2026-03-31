772b211c0446691a2aebe7004ccb653fb6a3107a
ali0223jjang <ali@inbl.io>
2026-03-31 10:19:20 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://cprtovccjnfuqaqtidyr.supabase.co';
-const SUPABASE_ANON_KEY = '여기에_anon_key_붙여넣기';
-const ADMIN_TOTP_SECRET = 'YOUR_TOTP_SECRET_HERE';
-const ADMIN_PW_HASH = 'YOUR_SHA256_HASH_HERE';
+const SUPABASE_ANON_KEY = 'sb_publishable_7v-ztehyDtlmF5FsjspZ_w_0wMSkfOO';
+const ADMIN_TOTP_SECRET = '6IW4JAJFWJWQD76KNMNSS525BROOP4AN';
+const ADMIN_PW_HASH = '9d560dd8e91864adf0d3bc5aad2aedeace108f2b1073718c5a2b9f82e03074b1';
